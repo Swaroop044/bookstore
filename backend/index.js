61adc13e87b6f12bcf0c7fc02ad376be75acaf37
Swaroop044 <dvvswaroop18@gmail.com>
2024-09-16 23:20:10 +0530

    book is saved and validated using postman

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,50 @@
 import express from "express";
-import {PORT, mongoDBURL} from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-app.get('/',(request,response)=>{
+app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome to MERN Stack Tutorial');
 });
 
+app.use(express.json());
 
+//Route to save a book
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: "Send all the required feils",
+            })
+        }
+        const newBook = {
+            title : request.body.title,
+            author : request.body.author,
+            publishYear : request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
 
 mongoose
     .connect(mongoDBURL)
-    .then(()=>{
+    .then(() => {
         console.log('App connected to database');
-        app.listen(PORT,()=>{
+        app.listen(PORT, () => {
             console.log(`App is listening to port: ${PORT}`);
         });
     })
-    .catch((error)=>{
+    .catch((error) => {
         console.log(error);
     });

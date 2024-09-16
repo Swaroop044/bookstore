399932c7e899b57f901822aa68648df825a44374
Swaroop044 <dvvswaroop18@gmail.com>
2024-09-16 22:59:29 +0530

    basic structure is done and bookmodel is created using mongoose library

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 9999;
+
+export const mongoDBURL =
+    'mongodb+srv://user:userpass@bookstore-mern.mb24b.mongodb.net/bookcollection?retryWrites=true&w=majority&appName=bookstore-mern';

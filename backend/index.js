5be612ed92fe21d031058fa1f255cf7b12e184b1
Swaroop044 <dvvswaroop18@gmail.com>
2024-09-17 12:56:05 +0530

    created react project with vite and tailwind CSS

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,23 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+//Middleware for handling CORS POLICY
+//Option 1: Allow Alll Origiins with Default of cors(*)
+app.use(cors());
+//Option 2: Allow Custom Origins
+app.use(
+    cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders : ['Content-Type'],
+})
+);
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome to MERN Stack Tutorial');

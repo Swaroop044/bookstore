3dbf7abb9c382e71158d5a98d6c448331b9ceb3c
Swaroop044 <dvvswaroop18@gmail.com>
2024-09-17 10:49:13 +0530

    get all books with mongoose

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -11,7 +11,7 @@ const bookSchema = mongoose.Schema(
             required : true,
         },
         publishYear: {
-            type: String,
+            type: Number,
             required : true,
         }
     },
@@ -20,4 +20,4 @@ const bookSchema = mongoose.Schema(
     }
 );
 
-export const Book = mongoose.model('Cat', { name: String });
+export const Book = mongoose.model("books",bookSchema);
